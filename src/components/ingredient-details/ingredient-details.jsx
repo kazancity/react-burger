d428aeab7a2fb64c8ab76c2fef4faaaa5e7ee175
kazancity <kazancity@aol.com>
2024-07-02 22:10:19 +0300

    refactor: refactoring ingredient-details.jsx file

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,37 +1,45 @@
+import { resetData } from "../../services/slices/ingredient-details-slice";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./ingredient-details.module.css";
-import { ingredientPropType } from "../../utils/types";
-import PropTypes from "prop-types";
+import { useEffect } from "react";
 
-const IngredientDetails = ({ ingredient }) => {
-  const { image_large, name, calories, proteins, fat, carbohydrates } =
-    ingredient;
+const IngredientDetails = () => {
+  const { name, image_large, calories, proteins, fat, carbohydrates } =
+    useSelector((store) => store.ingredientDetails.data);
+
+  const dispatch = useDispatch();
+  useEffect(() => () => dispatch(resetData()), []);
 
   return (
     <div className={styles.ingredient}>
-      <img src={image_large} alt={name} />
-      <span className={styles.text_main_medium}>{name}</span>
-      <div className={styles.nutrition}>
-        <div className={styles.nutrition_item}>
-          <span className={styles.text_main_default}>Калории, ккал</span>
-          <span className={styles.text_digits_default}>{calories}</span>
-        </div>
-        <div className={styles.nutrition_item}>
-          <span className={styles.text_main_default}>Белки, г</span>
-          <span className={styles.text_digits_default}>{proteins}</span>
-        </div>
-        <div className={styles.nutrition_item}>
-          <span className={styles.text_main_default}>Жиры, г</span>
-          <span className={styles.text_digits_default}>{fat}</span>
-        </div>
-        <div className={styles.nutrition_item}>
-          <span className={styles.text_main_default}>Углеводы, г</span>
-          <span className={styles.text_digits_default}>{carbohydrates}</span>
-        </div>
-      </div>
+      {name && (
+        <>
+          <img src={image_large} alt={name} />
+          <span className={styles.text_main_medium}>{name}</span>
+          <div className={styles.nutrition}>
+            <div className={styles.nutrition_item}>
+              <span className={styles.text_main_default}>Калории, ккал</span>
+              <span className={styles.text_digits_default}>{calories}</span>
+            </div>
+            <div className={styles.nutrition_item}>
+              <span className={styles.text_main_default}>Белки, г</span>
+              <span className={styles.text_digits_default}>{proteins}</span>
+            </div>
+            <div className={styles.nutrition_item}>
+              <span className={styles.text_main_default}>Жиры, г</span>
+              <span className={styles.text_digits_default}>{fat}</span>
+            </div>
+            <div className={styles.nutrition_item}>
+              <span className={styles.text_main_default}>Углеводы, г</span>
+              <span className={styles.text_digits_default}>
+                {carbohydrates}
+              </span>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
-
-IngredientDetails.propTypes = { ingredient: ingredientPropType };
 
 export default IngredientDetails;
