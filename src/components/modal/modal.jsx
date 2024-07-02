f3d5a429065ace54d55c37c806f2f1984a8cd7ef
kazancity <kazancity@aol.com>
2024-07-02 14:31:10 +0300

    refactor: refactoring modal.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,45 +1,40 @@
-import { useEffect } from "react";
-import { createPortal } from "react-dom";
 import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import ModalOverlay from "../modal-overlay/modal-overlay";
+import { useSelector } from "react-redux";
+import { createPortal } from "react-dom";
 import styles from "./modal.module.css";
 import PropTypes from "prop-types";
+import { useEffect } from "react";
 
 const Modal = ({ children, closeModWin, text }) => {
-  const rootBody = document.getElementById("modals");
+  const { isLoading } = useSelector((store) => store.orderDetails);
 
-  useEffect((_) => {
+  useEffect(() => {
     const closeEsc = (e) => {
       if (e.key === "Escape") closeModWin();
     };
     document.addEventListener("keydown", closeEsc);
-    return (_) => document.removeEventListener("keydown", closeEsc);
+    return () => document.removeEventListener("keydown", closeEsc);
   }, []);
 
   return createPortal(
     <>
       <ModalOverlay onClose={closeModWin} />
       <div className={styles.modal}>
-        {text && (
-          <h2
-            className={`${styles.header} ${styles.text} ${styles.text_main_large}`}
-          >
-            {text}
-          </h2>
-        )}
+        {text && <h2 className={styles.main}>{text}</h2>}
         <span className={styles.close} onClick={closeModWin}>
           <CloseIcon type="primary" />
         </span>
         {children}
       </div>
     </>,
-    rootBody,
+    document.getElementById("modals"),
   );
 };
 
 Modal.propTypes = {
-  children: PropTypes.element,
   closeModWin: PropTypes.func,
+  children: PropTypes.element,
   text: PropTypes.string,
 };
 
