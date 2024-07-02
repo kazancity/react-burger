94e21dee6490151a9f46d70017df873380eb64d0
kazancity <kazancity@aol.com>
2024-07-02 22:06:14 +0300

    refactor: refactoring burger-constructor-item-list.jsx file

--- src/components/burger-constructor-item-list/burger-constructor-item-list.jsx
+++ src/components/burger-constructor-item-list/burger-constructor-item-list.jsx
@@ -1,38 +1,99 @@
+import BurgerConstructorItemBase from "../burger-constructor-item-base/burger-constructor-item-base";
+import BurgerConstructorItemBun from "../burger-constructor-item-bun/burger-constructor-item-bun";
+import BurgerConstructorItem from "../burger-constructor-item/burger-constructor-item";
 import styles from "./burger-constructor-item-list.module.css";
-import ConstructorItem from "../burger-constructor-item/burger-constructor-item";
-import { ingredientPropType } from "../../utils/types";
+import { memo, useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import update from "immutability-helper";
+import { useDrop } from "react-dnd";
 import PropTypes from "prop-types";
 
-const ConstructorList = ({ ingredients }) => {
+const BurgerConstructorItemList = memo(function BurgerConstructorItemList({
+  dropHandler,
+}) {
+  const { bun, ingredients } = useSelector((store) => store.burgerConstructor);
+
+  const [constructorIngredients, setConstructorIngredients] =
+    useState(ingredients);
+
+  useEffect(() => {
+    setConstructorIngredients(ingredients);
+  }, [ingredients]);
+
+  const findIngredient = useCallback(
+    (id) => {
+      const ingredient = constructorIngredients.filter(
+        (ingredient) => ingredient.id === id,
+      )[0];
+      return {
+        ingredient,
+        index: constructorIngredients.indexOf(ingredient),
+      };
+    },
+    [constructorIngredients],
+  );
+
+  const moveIngredient = useCallback(
+    (id, atIndex) => {
+      const { ingredient, index } = findIngredient(id);
+      setConstructorIngredients(
+        update(constructorIngredients, {
+          $splice: [
+            [index, 1],
+            [atIndex, 0, ingredient],
+          ],
+        }),
+      );
+    },
+    [findIngredient, constructorIngredients],
+  );
+
+  const [, dropTarget] = useDrop({
+    accept: "ingredient",
+    drop(ingredient) {
+      dropHandler(ingredient);
+    },
+  });
+
+  const addBun = (position) =>
+    bun ? (
+      <BurgerConstructorItemBun
+        ingredient={bun}
+        position={position}
+        extraClass={styles.item_lock}
+      />
+    ) : (
+      <BurgerConstructorItemBase
+        text="Выберите булки"
+        position={position}
+        extraClass={styles.item_lock}
+      />
+    );
+
   return (
-    <section className={styles.item_section}>
-      <ConstructorItem
-        key={`${ingredients[0]._id}`}
-        ingredient={ingredients[0]}
-        type="top"
-        extraClass={styles.item_lock}
-      />
+    <section className={styles.item_section} ref={dropTarget}>
+      {addBun("top")}
       <ul className={styles.item_list}>
-        {ingredients
-          .filter((ingredient) => ingredient.type === "main")
-          .map((ingredient) => (
-            <li key={ingredient._id}>
-              <ConstructorItem ingredient={ingredient} />
-            </li>
-          ))}
+        {constructorIngredients.length ? (
+          constructorIngredients.map((ingredient) => (
+            <BurgerConstructorItem
+              key={ingredient.id}
+              ingredient={ingredient}
+              moveIngredient={moveIngredient}
+              findIngredient={findIngredient}
+            />
+          ))
+        ) : (
+          <BurgerConstructorItemBase text="Выберите начинку и соусы" />
+        )}
       </ul>
-      <ConstructorItem
-        key={`${ingredients[0]._id}+1`}
-        ingredient={ingredients[0]}
-        type="bottom"
-        extraClass={styles.item_lock}
-      />
+      {addBun("bottom")}
     </section>
   );
+});
+
+BurgerConstructorItemList.propTypes = {
+  dropHandler: PropTypes.func.isRequired,
 };
 
-ConstructorList.propTypes = {
-  ingredients: PropTypes.arrayOf(ingredientPropType),
-};
-
-export default ConstructorList;
+export default BurgerConstructorItemList;
