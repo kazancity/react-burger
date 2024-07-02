d972d2a729d7b032462c3fec7753cc4ddabe613a
kazancity <kazancity@aol.com>
2024-07-02 22:08:10 +0300

    refactor: refactoring burger-ingredients-item.jsx file

--- src/components/burger-ingredients-item/burger-ingredients-item.jsx
+++ src/components/burger-ingredients-item/burger-ingredients-item.jsx
@@ -3,22 +3,48 @@ import {
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import IngredientDetails from "../ingredient-details/ingredient-details";
+import { setData } from "../../services/slices/ingredient-details-slice";
 import useModalControl from "../../hooks/use-modal-control";
 import styles from "./burger-ingredients-item.module.css";
+import { useDispatch, useSelector } from "react-redux";
 import { ingredientPropType } from "../../utils/types";
+import { useDrag } from "react-dnd";
 import Modal from "../modal/modal";
-import PropTypes from "prop-types";
+import { useMemo } from "react";
 
-const IngredientItem = ({ ingredient, counter = null }) => {
+const BurgerIngredientsItem = ({ ingredient }) => {
+  const { bun, ingredients } = useSelector((store) => store.burgerConstructor);
+
+  const counter = useMemo(() => {
+    if (ingredient.type === "bun") {
+      return bun && bun._id === ingredient._id ? 2 : null;
+    }
+    return (
+      ingredients.filter((item) => item._id === ingredient._id).length || null
+    );
+  }, [bun, ingredients, ingredient.type, ingredient._id]);
+
   const { isShowModal, openModWin, closeModWin } = useModalControl(false);
   const { name, image, price } = ingredient;
+  const dispatch = useDispatch();
+  const [, dragRef] = useDrag({
+    type: "ingredient",
+    item: { ingredient },
+  });
+
+  const processShowIngredientDetails = () => {
+    dispatch(setData(ingredient));
+    openModWin();
+  };
 
   return (
     <>
-      <li className={styles.ingred_item} onClick={openModWin}>
-        {counter && (
-          <Counter count={counter} size="default" extraClass={styles.counter} />
-        )}
+      <li
+        className={styles.ingred_item}
+        onClick={processShowIngredientDetails}
+        ref={dragRef}
+      >
+        {counter && <Counter count={counter} size="default" />}
         <img src={image} alt={name} className={styles.ingred_margin} />
         <span className={styles.price_item}>
           {price}
@@ -28,16 +54,13 @@ const IngredientItem = ({ ingredient, counter = null }) => {
       </li>
       {isShowModal && (
         <Modal text="Детали ингредиента" closeModWin={closeModWin}>
-          <IngredientDetails ingredient={ingredient} />
+          <IngredientDetails />
         </Modal>
       )}
     </>
   );
 };
 
-IngredientItem.propTypes = {
-  ingredient: ingredientPropType,
-  counter: PropTypes.number,
-};
+BurgerIngredientsItem.propTypes = { ingredient: ingredientPropType };
 
-export default IngredientItem;
+export default BurgerIngredientsItem;
