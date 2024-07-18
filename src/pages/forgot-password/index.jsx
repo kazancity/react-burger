2033e4c1114875ca5413bd3c57d6f8ebbfb67704
kazancity <kazancity@aol.com>
2024-07-18 22:03:09 +0300

    add: add index.js for route forgot-password
    Signed-off-by: kazancity <kazancity@aol.com>

---
+++ src/pages/forgot-password/index.jsx
@@ -0,0 +1,51 @@
+import {
+  EmailInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { passwordResetRequest } from "../../utils/API";
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./forgot-password.module.css";
+import useForm from "../../hooks/use-form";
+
+export default function ForgotPasswordPage() {
+  const { formData, onChangeFormData, checkFormData } = useForm({ email: "" });
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (checkFormData.status) {
+      passwordResetRequest(formData).then((data) =>
+        navigate("/reset-password", { state: { message: data.message } }),
+      );
+    } else {
+      e.target
+        .querySelector(`[name=${checkFormData.field}]`)
+        .closest(".input")
+        .classList.add("input_status_error");
+    }
+  };
+
+  return (
+    <main className={`${styles.main}`}>
+      <h1 className="text text_type_main-medium">Восстановление пароля</h1>
+      <form className={`${styles.form} mt-6 mb-20`} onSubmit={handleSubmit}>
+        <EmailInput
+          onChange={onChangeFormData}
+          value={formData.email}
+          name="email"
+          isIcon={false}
+          placeholder="Укажите e-mail"
+        />
+        <Button htmlType="submit" type="primary" size="medium">
+          Восстановить
+        </Button>
+      </form>
+      <span className="text text_type_main-default text_color_inactive">
+        Вспомнили пароль?{" "}
+        <Link className={styles.blue} to="/login">
+          Войти
+        </Link>
+      </span>
+    </main>
+  );
+}
