ab7a8e428f53544eb69eb74605aea182a8a66ab7
kazancity <kazancity@aol.com>
2024-07-19 00:42:27 +0300

    fix: delete dispatch function as redundant from index.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/pages/ingredient/index.jsx
+++ src/pages/ingredient/index.jsx
@@ -1,16 +1,7 @@
 import IngredientDetails from "../../components/ingredient-details/ingredient-details";
-import { getIngredients } from "../../services/slices/burger-ingredients-slice";
 import styles from "./ingredient.module.css";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 export default function IngredientPage() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getIngredients());
-  }, [dispatch]);
-
   return (
     <main className={styles.ingredient}>
       <div className={styles.size}>
