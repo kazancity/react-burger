15a14325230fa8b465e5c5ef692c3e043f21eab9
kazancity <kazancity@aol.com>
2024-07-18 15:24:45 +0300

    refactor: refactor app.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,91 @@
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import {
+  MainPage,
+  LoginPage,
+  OrdersPage,
+  Page404,
+  ProfilePage,
+  RegisterPage,
+  IngredientPage,
+  ResetPasswordPage,
+  ForgotPasswordPage,
+} from "../../pages";
+import {
+  RouteOnlyAuth,
+  RouteOnlyUnAuth,
+} from "../protected-route/protected-route";
+import { useEffect } from "react";
+import Modal from "../modal/modal";
+import { useDispatch } from "react-redux";
 import AppHeader from "../app-header/app-header";
-import { DndProvider } from "react-dnd";
-import styles from "./app.module.css";
+import OrderDetails from "../order-details/order-details";
+import { checkUserAuth } from "../../services/slices/user-slice";
+import IngredientDetails from "../ingredient-details/ingredient-details";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(checkUserAuth());
+  }, [dispatch]);
+
   return (
-    <div className={styles.app}>
+    <>
       <AppHeader />
-      <main className={styles.main}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </DndProvider>
-      </main>
-    </div>
+      <Routes location={location.state?.backgroundLocation || location}>
+        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/login"
+          element={<RouteOnlyUnAuth component={<LoginPage />} />}
+        />
+        <Route
+          path="/register"
+          element={<RouteOnlyUnAuth component={<RegisterPage />} />}
+        />
+        <Route
+          path="/forgot-password"
+          element={<RouteOnlyUnAuth component={<ForgotPasswordPage />} />}
+        />
+        <Route
+          path="/reset-password"
+          element={<RouteOnlyUnAuth component={<ResetPasswordPage />} />}
+        />
+        <Route
+          path="/profile"
+          element={<RouteOnlyAuth component={<ProfilePage />} />}
+        >
+          <Route
+            path="orders"
+            element={<RouteOnlyAuth component={<OrdersPage />} />}
+          />
+        </Route>
+        <Route path="/ingredients/:id" element={<IngredientPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+
+      {location.state?.backgroundLocation && (
+        <Routes>
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal onClose={() => navigate("/", { replace: true })}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Modal onClose={() => navigate("/", { replace: true })}>
+                <OrderDetails />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </>
   );
 }
 
