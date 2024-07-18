ef50a3b24d86a9b66f68dd07b9392602b6028143
kazancity <kazancity@aol.com>
2024-07-18 15:25:58 +0300

    refactor: refactor app-header.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,8 +1,16 @@
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
-import AppHeaderItem from "../app-header-item/app-header-item";
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import styles from "./app-header.module.css";
 
 const AppHeader = () => {
+  const { user } = useSelector((store) => store.user);
+
   return (
     <header className={styles.header}>
       <span className={styles.logo}>
@@ -10,9 +18,57 @@ const AppHeader = () => {
       </span>
       <nav>
         <ul className={styles.list}>
-          <AppHeaderItem iconMenu="burger" textMenu="Конструктор" isActive />
-          <AppHeaderItem iconMenu="list" textMenu="Лента заказов" />
-          <AppHeaderItem iconMenu="profile" textMenu="Личный кабинет" />
+          <li className={styles.last_item}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.link}`
+                  : `${styles.link} ${styles.inactive}`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <BurgerIcon type={isActive ? "primary" : "secondary"} />
+                  {"Конструктор"}
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li className={styles.last_item}>
+            <NavLink
+              to="/list"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.link}`
+                  : `${styles.link} ${styles.inactive}`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <ListIcon type={isActive ? "primary" : "secondary"} />
+                  {"Лента заказов"}
+                </>
+              )}
+            </NavLink>
+          </li>
+          <li className={styles.last_item}>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles.link}`
+                  : `${styles.link} ${styles.inactive}`
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <ProfileIcon type={isActive ? "primary" : "secondary"} />
+                  {user ? user.name : "Личный кабинет"}
+                </>
+              )}
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
