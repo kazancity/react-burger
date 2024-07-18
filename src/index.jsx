baad11cf315a41e17b1922adfcc27a8ae997576e
kazancity <kazancity@aol.com>
2024-07-18 14:26:22 +0300

    refactor: changing the index.jsx file structure
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,17 @@
 import "./index.css";
-import React from "react";
 import store from "./services/store";
 import { Provider } from "react-redux";
 import App from "./components/app/app";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
 
