29b7769d64d6db12db6ebb9f278d7fd29291ee49
kazancity <kazancity@aol.com>
2024-07-18 15:57:04 +0300

    refactor: refactor order-details.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,17 +1,20 @@
 import { clearOrder } from "../../services/slices/order-details-slice";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./order-details.module.css";
+import { Navigate } from "react-router-dom";
 import done from "../../images/done.png";
 import { useEffect } from "react";
 
 const OrderDetails = () => {
   const { isError, data } = useSelector((store) => store.orderDetails);
+
   const dispatch = useDispatch();
+  // eslint-disable-next-line
   useEffect(() => () => dispatch(clearOrder()), []);
 
   return (
     <div className={styles.order}>
-      {data && (
+      {data ? (
         <>
           <span className={styles.number}>{data.order.number}</span>
           <span className={styles.text_main_medium}>идентификатор заказа</span>
@@ -23,6 +26,8 @@ const OrderDetails = () => {
             Дождитесь готовности на орбитальной станции
           </span>
         </>
+      ) : (
+        <Navigate to="/" replace={true} />
       )}
       {isError && <>В процессе отправки заказа произошла ошибка!</>}
     </div>
