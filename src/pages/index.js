2845a506eb86e23a068872ccd3e3e3b3b393863e
kazancity <kazancity@aol.com>
2024-07-18 22:02:06 +0300

    add: add index.js for route
    Signed-off-by: kazancity <kazancity@aol.com>

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import MainPage from "./main";
+import LoginPage from "./login";
+import Page404 from "./page-404";
+import OrdersPage from "./orders";
+import ProfilePage from "./profile";
+import RegisterPage from "./register";
+import IngredientPage from "./ingredient";
+import ResetPasswordPage from "./reset-password";
+import ForgotPasswordPage from "./forgot-password";
+
+export {
+  Page404,
+  MainPage,
+  LoginPage,
+  OrdersPage,
+  ProfilePage,
+  RegisterPage,
+  IngredientPage,
+  ResetPasswordPage,
+  ForgotPasswordPage,
+};
