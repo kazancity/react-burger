709363061a10a2dad9032c08c87272652136502b
kazancity <kazancity@aol.com>
2024-08-05 17:13:51 +0300

    add: add modal.tsx file
    Signed-off-by: kazancity <kazancity@aol.com>

---
+++ src/components/modal/modal.tsx
@@ -0,0 +1,61 @@
+import { CloseIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { PropsWithChildren, ReactPortal, useEffect } from "react";
+import ModalOverlay from "../modal-overlay/modal-overlay";
+import { GridLoader } from "react-spinners";
+import { useSelector } from "react-redux";
+import { createPortal } from "react-dom";
+import styles from "./modal.module.css";
+import { Store } from "../../types";
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+const Modal = ({
+  children,
+  onClose,
+}: PropsWithChildren<ModalProps>): ReactPortal => {
+  const { isLoading } = useSelector((store: Store) => store.orderDetails);
+
+  useEffect(() => {
+    const closeEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+    document.addEventListener("keydown", closeEsc);
+    return () => document.removeEventListener("keydown", closeEsc);
+    // eslint-disable-next-line
+  }, []);
+
+  return createPortal(
+    <>
+      {isLoading ? (
+        <>
+          <ModalOverlay />
+          <GridLoader
+            color="#8585ad"
+            loading={isLoading}
+            cssOverride={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate('-50%', '-50%')",
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <ModalOverlay onClick={onClose} />
+          <div className={styles.modal}>
+            <span className={styles.close} onClick={onClose}>
+              <CloseIcon type="primary" />
+            </span>
+            {children}
+          </div>
+        </>
+      )}
+    </>,
+    document.getElementById("modals") as HTMLElement,
+  );
+};
+
+export default Modal;
