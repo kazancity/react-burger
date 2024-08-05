b6385555a837ddfc7ac028d2715c953fb0a7dc82
kazancity <kazancity@aol.com>
2024-08-05 16:34:25 +0300

    add: add burger-constructor-item-bun.tsx file
    Signed-off-by: kazancity <kazancity@aol.com>

---
+++ src/components/burger-constructor-item-bun/burger-constructor-item-bun.tsx
@@ -0,0 +1,31 @@
+import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./burger-constructor-item-bun.module.css";
+import { Ingredient, BurgerConstructorItemType } from "../../types";
+import { FC } from "react";
+
+interface BurgerConstructorItemBunProps {
+  ingredient: Ingredient;
+  type: BurgerConstructorItemType;
+  extraClass?: string;
+}
+
+const BurgerConstructorItemBun: FC<BurgerConstructorItemBunProps> = ({
+  ingredient,
+  type,
+  extraClass = "",
+}) => {
+  return (
+    <div className={`${styles.item} ${extraClass}`}>
+      <ConstructorElement
+        text={`${ingredient.name}${type === "top" ? " (верх)" : " (низ)"}`}
+        price={ingredient.price}
+        thumbnail={ingredient.image_mobile}
+        type={type}
+        isLocked={true}
+        extraClass={styles.element}
+      />
+    </div>
+  );
+};
+
+export default BurgerConstructorItemBun;
