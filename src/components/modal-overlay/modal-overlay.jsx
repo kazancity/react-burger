9bf460554b5c5461b92f6a685fd67565d5598e75
kazancity <kazancity@aol.com>
2024-08-05 17:16:10 +0300

    delete: delete modal-overlay.jsx file
    Signed-off-by: kazancity <kazancity@aol.com>

--- src/components/modal-overlay/modal-overlay.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./modal-overlay.module.css";
-import PropTypes from "prop-types";
-
-const ModalOverlay = ({ onClick }) => (
-  <div className={styles.overlay} onClick={onClick}></div>
-);
-ModalOverlay.propTypes = { onClick: PropTypes.func };
-
-export default ModalOverlay;
