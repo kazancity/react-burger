55e51c634d0eecd055aa9038043cd42933175173
kazancity <kazancity@aol.com>
2024-08-23 15:26:41 +0300

    refactoring: refactoring orders page for loading ingredients

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,3 +1,43 @@
+import { connect, disconnect } from "../../services/slices/websocket-slice";
+import { useDispatch, useSelector } from "../../hooks/hooks-types";
+import OrderList from "../../components/order-list";
+import { WebSocketStatus } from "../../types";
+import { GridLoader } from "react-spinners";
+import styles from "./orders.module.css";
+import { useEffect } from "react";
+
 export default function OrdersPage() {
-  return <h1>Тут будет история заказов</h1>;
+  const dispatch = useDispatch();
+  const { status, orders } = useSelector((store) => store.webSocket);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken)
+      dispatch(connect(`orders?token=${accessToken.substring(7)}`));
+    return () => {
+      dispatch(disconnect());
+    };
+  }, [dispatch]);
+
+  return (
+    <div className={styles.orders}>
+      <GridLoader
+        color="#8585ad"
+        loading={status !== WebSocketStatus.ONLINE}
+        cssOverride={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate('-50%', '-50%')",
+        }}
+      />
+      {status === WebSocketStatus.ONLINE && orders.length > 0 && (
+        <OrderList
+          isShowStatus={true}
+          linkEndpoint="/profile/orders"
+          isOrdersReverse={true}
+        />
+      )}
+    </div>
+  );
 }
